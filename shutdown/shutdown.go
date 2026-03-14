60a1f8de0fe475ce822c04b286cd996c5524c98c
Anton <161073380+Anton9372@users.noreply.github.com>
2026-03-14 21:17:14 +0300

    Reverse clone connections order (#9)
    * Reverse clone connections order
    
    * Fix
    
    * Fix 2

--- shutdown/shutdown.go
+++ shutdown/shutdown.go
@@ -9,32 +9,54 @@ import (
 	"github.com/Anton9372/go-lib/logger"
 )
 
+type CloseStage []CloseFunc
+
 type CloseFunc func(context.Context) error
 
-func CloseConnections(closers []CloseFunc, l *slog.Logger, timeout time.Duration) {
+func CloseConnections(stages []CloseStage, l *slog.Logger, timeout time.Duration) {
 	l.Warn("Closing connections", slog.Duration("timeout", timeout))
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	var wg sync.WaitGroup
-	wg.Add(len(closers))
-
-	for _, closeFn := range closers {
-		go func(fn func(context.Context) error) {
-			defer wg.Done()
-
-			err := fn(ctx)
-			if err != nil {
-				l.Error("Closing connection", logger.ErrAttr(err))
-			}
-		}(closeFn)
-	}
-
 	done := make(chan struct{})
+
 	go func() {
-		wg.Wait()
-		done <- struct{}{}
+		defer close(done)
+
+		for i := len(stages) - 1; i >= 0; i-- {
+			stage := stages[i]
+			if len(stage) == 0 {
+				continue
+			}
+
+			l.Debug("Starting shutdown stage", slog.Int("stage_index", i), slog.Int("closers", len(stage)))
+
+			var wg sync.WaitGroup
+			wg.Add(len(stage))
+
+			for _, closeFn := range stage {
+				go func(fn CloseFunc) {
+					defer wg.Done()
+					if err := fn(ctx); err != nil {
+						l.Error("Closing connection", logger.ErrAttr(err))
+					}
+				}(closeFn)
+			}
+
+			stageDone := make(chan struct{})
+			go func() {
+				wg.Wait()
+				close(stageDone)
+			}()
+
+			select {
+			case <-stageDone:
+			case <-ctx.Done():
+				l.Error("Stage shutdown interrupted by timeout", slog.Int("stage_index", i))
+				return
+			}
+		}
 	}()
 
 	select {
