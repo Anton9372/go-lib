7c7dac8d024799f19fa4debe9fbfed5de8882974
Anton9372 <therealbigboyslime@gmail.com>
2026-03-14 21:00:30 +0300

    Fix

--- shutdown/shutdown.go
+++ shutdown/shutdown.go
@@ -9,31 +9,45 @@ import (
 	"github.com/Anton9372/go-lib/logger"
 )
 
+type CloseStage []CloseFunc
+
 type CloseFunc func(context.Context) error
 
-func CloseConnections(closers []CloseFunc, l *slog.Logger, timeout time.Duration) {
+func CloseConnections(stages []CloseStage, l *slog.Logger, timeout time.Duration) {
 	l.Warn("Closing connections", slog.Duration("timeout", timeout))
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	var wg sync.WaitGroup
-	wg.Add(len(closers))
-
-	for i := len(closers) - 1; i >= 0; i-- {
-		go func(fn func(context.Context) error) {
-			defer wg.Done()
-
-			err := fn(ctx)
-			if err != nil {
-				l.Error("Closing connection", logger.ErrAttr(err))
-			}
-		}(closers[i])
-	}
-
 	done := make(chan struct{})
+
 	go func() {
-		wg.Wait()
+		for i, stage := range stages {
+			if len(stage) == 0 {
+				continue
+			}
+
+			l.Debug("Starting shutdown stage", slog.Int("stage_index", i), slog.Int("closers", len(stage)))
+
+			var wg sync.WaitGroup
+			wg.Add(len(stage))
+
+			for _, closeFn := range stage {
+				go func(fn CloseFunc) {
+					defer wg.Done()
+					if err := fn(ctx); err != nil {
+						l.Error("Closing connection", logger.ErrAttr(err))
+					}
+				}(closeFn)
+			}
+
+			wg.Wait()
+
+			if ctx.Err() != nil {
+				break
+			}
+		}
+
 		done <- struct{}{}
 	}()
 
