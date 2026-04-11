3e64363a8397c1609d4d746d41d69dc744a9e462
Anton <161073380+Anton9372@users.noreply.github.com>
2026-04-11 22:50:17 +0300

    feat: HTTP Headers (#12)

---
+++ restapi/headers.go
@@ -0,0 +1,15 @@
+package restapi
+
+const (
+	HeaderAuthorization = "Authorization"
+
+	HeaderXUserID        = "X-User-ID"
+	HeaderXUserRole      = "X-User-Role"
+	HeaderXRealIP        = "X-Real-IP"
+	HeaderXCorrelationID = "X-Correlation-ID"
+
+	HeaderRateLimitLimit     = "RateLimit-Limit"
+	HeaderRateLimitRemaining = "RateLimit-Remaining"
+	HeaderRateLimitReset     = "RateLimit-Reset"
+	HeaderRetryAfter         = "Retry-After"
+)
